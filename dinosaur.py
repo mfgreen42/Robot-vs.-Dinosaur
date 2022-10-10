0e5a2ed8588d665726eb162c09b9be27a3d37d54
Mara Green <maragreen42@gmail.com>
2022-10-10 14:01:10 -0700

    added robot, weapon, dinosaur and battlefield class

---
+++ dinosaur.py
@@ -0,0 +1,12 @@
+
+class Dinosaur:
+
+
+    def __init__(self, name, attack_power):
+        self.name = name
+        self.attack_power = attack_power #int
+        self.health = 100
+
+
+    def attack(self, robot):
+        pass
---
+++ weapon.py
@@ -0,0 +1,6 @@
+
+class Weapon:
+
+    def __init__(self, name, attack_power):
+        self.name = name #string
+        self.attack_power = attack_power #int
