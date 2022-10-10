f1d2194cdfaf578a30e853707f8ea66006a32775
Mara Green <maragreen42@gmail.com>
2022-10-10 14:08:26 -0700

    add details to classes

---
+++ battlefield.py
@@ -0,0 +1,27 @@
+
+from robot import Robot
+from dinosaur import Dinosaur
+
+
+class Battlefield:
+
+    def __init__(self):
+        self.robot = Robot
+        self.dinosaur = Dinosaur
+
+    def run_game(self):
+        pass
+
+
+    def display_welcome(self):
+        pass
+
+    def battle_phase(self):
+        pass
+
+    def display_winner(self):
+        pass
+
+
+
+
